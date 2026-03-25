8087e6d213f0b5430093acd5ffd8c8fd7bae3c66
7twoduo <152588192+7twoduo@users.noreply.github.com>
2026-03-25 03:48:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,19 @@
-provider "aws" {
-  region = "us-east-1"
-} 
+terraform {
+  backend "s3" {
+    bucket       = "lion-rari"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    #use_lockfile = true
+  }
+}
+
+
 
 resource "aws_vpc" "shadows" {
-
-  cidr_block       = "10.20.0.0/16"
-
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
 
   tags = {
     Name = "2-shadows"
   }
-} 
+}
