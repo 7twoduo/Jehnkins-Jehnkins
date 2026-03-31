6a8911d442e528544317ee70b026631613bca2b2
Gavin Forge <152588192+7twoduo@users.noreply.github.com>
2026-03-31 15:46:52 -0400

    Forteenth

--- main.tf
+++ main.tf
@@ -18,6 +18,14 @@ terraform {
   }
 }
 
+resource "aws_vpc" "shadows" {
+  cidr_block = "10.2.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "2-shadows"
+  }
+}
      
    
 provider "aws" {
